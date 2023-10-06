3f4813c34cd881596329b415ab522c1ca493854c
igorsmotto <igor@mobile.dev>
2023-10-06 10:01:48 -0300

    Version 0.6

--- dist/index.js
+++ dist/index.js
@@ -47249,7 +47249,7 @@ function getParameters() {
         const excludeTags = parseTags(core.getInput('exclude-tags', { required: false }));
         const appFilePath = core.getInput('app-file', { required: false });
         const appBinaryId = core.getInput('app-binary-id', { required: false });
-        if ((appFilePath !== "") !== (appBinaryId !== "")) {
+        if (!(appFilePath !== "") !== (appBinaryId !== "")) {
             throw new Error("Either app-file or app-binary-id must be used");
         }
         var env = {};
